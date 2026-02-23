4234c14b21d956acc702338dfb41545c7d8ddc2e
Simran Sharma <ss1043422@gmail.com>
2026-02-23 17:27:47 +0530

    structure

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ROUTES from '../../constant/Route/route'
+import { Avatar } from '@mui/material'
 
 function Navbar() {
   return (
     <div>
-      
+      {/* image */}
+      <img src="/ConnectX.png" alt="profile-image" height={80} width={80} />
+      {/* search bar (for user) */}
+      <input type="text" className='input-box' placeholder='search user' />
+      {/* routes */}
+      <div className="link-list">
+        <Link to={ROUTES.HOME} className='link-info'>Home</Link>
+        <Link to={ROUTES.NOTIFICATION} className='link-info'>Notifications</Link>
+        <Link to={ROUTES.MESSAGES} className='link-info'>Messages</Link>
+        <Link to={ROUTES.PROFILE} className='link-info'>
+          <Avatar />
+        </Link>
+
+      </div>
+      {/* home , messages , notification (notification idea to delete after 7 days or 30 days) 
+      while getting notification */}
+      {/* avatar (onClick - profile) */}
     </div>
   )
 }
--- Frontend/src/pages/Profile/Profile.jsx
+++ Frontend/src/pages/Profile/Profile.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import { useState } from 'react'
+import Post from '../../components/Modals/Post/Post';
 
 function Profile() {
+  const [openPost, setOpenPost] = useState(false);
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <div className="profile-conainer">
+        <Sidebar />
+        {/* right side */}
+        {/* followers , follwing , new post  */}
+        <button onClick={() => setOpenPost(true)}>New Post</button>
+        {openPost &&
+          <Post
+            open={() => setOpenPost(true)}
+            onClose={() => setOpenPost(false)}
+          />}
+        {/* all post of mine */}
+      </div>
+    </>
   )
 }
 
